fd501683eb0dac956ad0854a8b7a4736b15f02f2
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-25 17:53:56 +0500

    Fixed indentation in usre controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def index
+  end
+  def edit
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to dashboard_root_path, notice: 'User deleted.'
+  end
+end
