72cf4d5725cc952badccb448ead51179e550e57e
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-25 11:24:30 +0500

    Indendation fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-         def configure_permitted_parameters
-              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :type)}
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:name, :email, :password, :type)}
 
-              devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :type, :password, :current_password)}
-         end
+    devise_parameter_sanitizer.permit(:account_update) { |user| user.permit(:name, :email, :type, :password, :current_password)}
+  end
 
 end
 
- 
+
