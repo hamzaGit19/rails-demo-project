0c37be8121fb95c5f9f23003dbcb01620e29d06b
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-25 17:39:54 +0500

    Fixed indenation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,12 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:name, :email, :password, :type) }
-
     devise_parameter_sanitizer.permit(:account_update) { |user| user.permit(:name, :email, :type, :password, :current_password) }
   end
 end
