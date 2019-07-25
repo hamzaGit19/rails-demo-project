72320d09973fe6eda065103ad6997a33e2e93615
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-25 17:31:48 +0500

    Fixed indenation

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'users/index'
+  match 'users/:id' => 'users#destroy', :via => :delete, :as => :user_delete
   devise_for :users
-  # devise_for :managers
-  # devise_for :client
-  # devise_for :admins
-
   namespace :dashboard do
     get 'dashboard/index'
   end
   get 'dashboard/index'
   root to: 'pages#index'
-
   namespace :dashboard do
     root to: 'dashboard#index'
   end
-
+  resources :users do
+    member do
+      patch :toggle_enable_status
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_724_070_236) do
+ActiveRecord::Schema.define(version: 2019_07_25_071657) do
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20_190_724_070_236) do
     t.string 'type'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.boolean 'status', default: false
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
