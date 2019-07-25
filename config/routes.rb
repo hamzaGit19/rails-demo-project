bd1bfaa388357b5736c69ef4dcf5c0d16b61343a
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-25 18:02:46 +0500

    Added namespace Admin -> Users in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,8 @@ Rails.application.routes.draw do
   namespace :dashboard do
     root to: 'dashboard#index'
   end
-  resources :users do
-    member do
-      patch :toggle_enable_status
-    end
+  namespace :admin do 
+    resources :users
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
