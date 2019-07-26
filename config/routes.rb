efc2a9bfaef89314b1f808879ec876e67a46a6fd
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-26 16:33:29 +0500

    fixed a typo in config file

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   namespace :admin do
     match ':id' => 'users#destroy', :via => :delete, :as => :user_delete
     # match ':id(.:format)' => 'users#update', :via => :patch, :as=> :edit_admin_user
-    resources :userss
+    resources :users
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
