7ca694c57a098ee29992b6a8b2a1a966dfd633db
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-26 16:27:48 +0500

    Removed unnecessary code for destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,8 @@
 class UsersController < ApplicationController
   def index
   end
+
   def edit
   end
-
-  # def destroy
-  #   sign_out :current_user
-  #   # @user = User.find(params[:id])
-  #   # @user.destroy
-  #   redirect_to dashboard_root_path, notice: 'sign out.'
-  # end
+ 
 end
