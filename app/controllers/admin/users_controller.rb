425fbeeb42c48d40be66857e637558e36c6f68fb
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-26 16:25:48 +0500

    Added logic for admin - user controller  for functionality : Admin can update any user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,21 +7,34 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to dashboard_root_path, notice: 'user was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def edit 
+  def edit
   end
 
   def destroy
     @user.destroy
-    redirect_to dashboard_root_path, notice: "User deleted."
+    redirect_to dashboard_root_path, notice: 'User deleted.'
   end
 
   def user_params
-    params.require(:users).permit(:name, :email, :type, :id)
+    params.require(klass.to_s.underscore).permit(:name, :email, :id)
   end
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def klass
+    @user.class
   end
 end
