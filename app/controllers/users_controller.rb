7f123cd0361a04e4848a2f7628619ff1b983e2ed
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-26 20:27:53 +0500

    Added show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy, :show]
+
   def index
   end
 
   def edit
+  end
+
+  def show 
+  end
+
+
+  def set_user
+    @user = current_user
   end
  
 end
