486618a81f90f4819bf5f40096d961fd43440baa
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-26 21:32:46 +0500

    Created action update password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :destroy, :show]
+  before_action :set_user
 
   def index
   end
@@ -12,9 +12,26 @@ class UsersController < ApplicationController
   def show 
   end
 
+  def update_password
+    @user = current_user
+    if @user.update(password_params)
+      # Sign in the user by passing validation in case their password changed
+      bypass_sign_in(@user)
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end
 
   def set_user
     @user = current_user
   end
- 
+  def password_params
+    # NOTE: Using `strong_parameters` gem
+    params.require(klass.to_s.underscore).permit(:password, :password_confirmation)
+  end
+
+  def klass
+    @user.class
+  end
 end
