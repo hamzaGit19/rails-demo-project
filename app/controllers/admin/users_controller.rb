6c73dd9e61f9024de88a37e45e9fa6264c87cc94
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-26 11:04:03 +0500

    Updates Admin-usercontroller and user-view to add Delete a user functionality

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,24 @@
+# frozen_string_literal: true
+
 class Admin::UsersController < ApplicationController
-    def index
-    end
+  before_action :set_user, only: [:edit, :update, :destroy]
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to dashboard_root_path, notice: "User deleted."
+  end
+
+  def user_params
+    params.require(:users).permit(:name, :email, :type, :user_id)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
