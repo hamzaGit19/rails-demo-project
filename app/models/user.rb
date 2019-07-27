5c7b249b872fa0db17593a757e724dbea64d9ac0
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-27 19:32:21 +0500

    Added  Create new user button on admin dashboard and changed pagination to 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
-    paginates_per 1
+    paginates_per 7
 
   def admin?
     instance_of?(Admin)
