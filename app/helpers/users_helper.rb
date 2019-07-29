a628803f160c09ce80ee1db749d0cca154afd9e5
hamzaGit19 <l154144@lhr.nu.edu.pk>
2019-07-29 12:07:36 +0500

    Modified helpers for user registeration

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,12 @@
 
 module UsersHelper
   def options_for_user_types
-    [%w[Admin],
-     %w[Client], %w[Manager], %w[Employee]]
+ 
+    if current_user.admin?
+      options = [%w[Admin],
+                 %w[Client], %w[Manager], %w[Employee]]
+    elsif current_user.manager?
+      options = [%w[Client], %w[Employee]]
+    end
   end
-
 end
